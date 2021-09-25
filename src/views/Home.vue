8a0b5dde4549cde08441ce67c2b6453bbf073ddf
Machalka <al1999dk@gmail.com>
2021-09-26 01:36:51 +0300

    сделал подключение через wrbsockets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
                     <div class="col-md-6 themed-grid-col">
                         <h1 class="text-center fw-normal display-1">GreenHouse</h1>
                         <img src="../assets/svg/043-hydroponic.svg" class="mt-2 mb-3 center-element" alt="GreenHouse" width="400em">
-                        <p class="lead fs-5 fw-normal text-center">Добро пожаловать на сайт дипломного проекта сдудента ДГТУ Ячменева Ярослава Владимировича.</p>
+                        <p class="lead fs-5 fw-normal text-center">Добро пожаловать!</p>
                         <p class="lead fs-5 fw-normal text-center">Для того, чтобы зайти в панель управления</p>
                         <router-link to="/user/auth" class="btn btn-outline-success btn-lg center-element" style="width: fit-content;">Авторизуйтесь</router-link>
                         <p class="lead fs-5 fw-normal text-center mt-1">или зайдите </p>
